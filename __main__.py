7f68aa264eca4f2a395156634833a9e74d2765c4
glitchy <thepatheticweebgamer@gmail.com>
2022-06-28 23:04:41 -0400

    :sparkles: feat: added ability to easily specify dev bot from main bot

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
-from naff import Intents
+import sys
 
-from config import TOKEN
+from naff import Intents
+from secrets import TOKEN, DEV_TOKEN
 from core import Megumin
 
 if __name__ == "__main__":
     client = Megumin(intents=Intents.ALL)
-    client.start(TOKEN)
+    if len(sys.argv) > 1 and sys.argv[1] == 'dev':
+        client.start(DEV_TOKEN)
+    elif len(sys.argv) < 1 or sys.argv[1] == 'main':
+        client.start(TOKEN)
+    else:
+        client.logger.error("Unknown arg given on startup")
